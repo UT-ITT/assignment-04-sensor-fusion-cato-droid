4837d600d5ce599a7bb33e6f77e4068378298673
Cato Kurtz <cato.kurtz@student.uni-tuebingen.de>
2026-06-14 19:18:34 +0200

    finished game mechanics

--- ar_game/AR_game.py
+++ ar_game/AR_game.py
@@ -3,11 +3,21 @@ import cv2.aruco as aruco
 import sys
 import numpy as np
 import pyglet
-from pyglet import window, shapes
+from pyglet import window, shapes, font
 from PIL import Image
 import sys
 import random
+import time
 
+#load cool font (https://www.1001fonts.com/pixel-game-font.html)
+font.add_file('Pixel Game.otf')
+
+#global variables
+markers_detected = False
+end = False
+score = 0
+
+#video source
 video_id = 0
 
 if len(sys.argv) > 1:
@@ -18,7 +28,6 @@ aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 aruco_params = aruco.DetectorParameters()
 detector = aruco.ArucoDetector(aruco_dict, aruco_params)
 
-markers_detected = False
 
 # Create a video capture object for the webcam
 cap = cv2.VideoCapture(video_id)
@@ -28,6 +37,7 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 win = window.Window(width, height, caption="Cool Video Game")
 
 #shapes for the game
+#paddle
 paddle_width = max(width/10, 20)
 paddle_height = max(height/40, 5)
 
@@ -37,6 +47,7 @@ paddle = shapes.Rectangle(width//2 - paddle_width//2,
                           paddle_height,
                           color = (0, 255, 0))
 
+#ball
 ball_radius = paddle_width/4
 
 ball = shapes.Circle(width//2, 
@@ -44,9 +55,11 @@ ball = shapes.Circle(width//2,
                      ball_radius,
                      color=(255, 0, 0))
 
-ball_vx = 100
-ball_vy = 100
+#speed in x and y direction
+ball_vx = 8
+ball_vy = 8
 
+#bricks
 bricks = []
 
 rows = 5
@@ -129,73 +142,149 @@ def get_tip(contour):
 
 @win.event
 def on_draw():
-    global markers_detected
+    global markers_detected, end, ball_vx, ball_vy, score
 
-    # Capture a frame from the webcam
-    ret, frame = cap.read()
-
-    if ret:
-        # Convert the frame to grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Detect ArUco markers in the frame
-        corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
-
-        # Check if marker is detected
-        if ids is not None:
-            #print(f"ids: {ids}") #FIXME remove test print statements
-            #print(f"corners: {corners}")
-            # Draw lines along the sides of the marker
-            aruco.drawDetectedMarkers(frame, corners)
-            #start transformation if all 4 markers are successfully captured
-            if len(ids) == 4:
-                markers_detected = True
-                points = [[0,0], [0,0], [0,0], [0,0]]
-                #order marker points to put the correct corner values in points for the transition to work
-                for i in range (len(ids)):
-                    if ids[i] == [0]:
-                        points[0] = corners[i][0][3]
-                    elif ids[i] == [1]:
-                        points[1] = corners[i][0][0]
-                    elif ids[i] == [2]:
-                        points[2] = corners[i][0][2]
-                    elif ids[i] == [3]:
-                        points[3] = corners[i][0][1]
-
-                #inner corners of all 4 markers
-                points = np.float32(points)
-                #their position on the transformed frame
-                points_2 = np.float32([[0, height], [0, 0], [width, height], [width, 0]])
-
-                #transform frame to display the rectangle between the markers with camera's resolution
-                M = cv2.getPerspectiveTransform(points, points_2)
-                frame = cv2.warpPerspective(frame, M, (width, height))
-
-                #finger detection
-                mask = get_mask(frame)
-                contour = get_object_contour(mask)
-                if contour is not None:
-                    tip = get_tip(contour)
-                    x,y = tip
-
-                    #game mechanics
-                    paddle.x = x - paddle_width/2
-                    paddle.x = max(0, min(width - paddle_width, paddle.x))
-
-            else:
-                markers_detected = False
+    if not end: 
 
 
+        # Capture a frame from the webcam
+        ret, frame = cap.read()
 
-        # Display the frame
+        if ret:
+            # Convert the frame to grayscale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Detect ArUco markers in the frame
+            corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
+
+            # Check if marker is detected
+            if ids is not None:
+                # Draw lines along the sides of the marker
+                aruco.drawDetectedMarkers(frame, corners)
+                #start transformation if all 4 markers are successfully captured
+                if len(ids) == 4:
+                    markers_detected = True
+                    points = [[0,0], [0,0], [0,0], [0,0]]
+                    #order marker points to put the correct corner values in points for the transition to work
+                    for i in range (len(ids)):
+                        if ids[i] == [0]:
+                            points[0] = corners[i][0][3]
+                        elif ids[i] == [1]:
+                            points[1] = corners[i][0][0]
+                        elif ids[i] == [2]:
+                            points[2] = corners[i][0][2]
+                        elif ids[i] == [3]:
+                            points[3] = corners[i][0][1]
+
+                    #inner corners of all 4 markers
+                    points = np.float32(points)
+                    #their position on the transformed frame
+                    points_2 = np.float32([[0, height], [0, 0], [width, height], [width, 0]])
+
+                    #transform frame to display the rectangle between the markers with camera's resolution
+                    M = cv2.getPerspectiveTransform(points, points_2)
+                    frame = cv2.warpPerspective(frame, M, (width, height))
+
+                    #finger detection
+                    mask = get_mask(frame)
+                    contour = get_object_contour(mask)
+                    if contour is not None:
+                        tip = get_tip(contour)
+                        x,y = tip
+
+                        #game mechanics
+                        #paddle follows finger
+                        paddle.x = x - paddle_width/2
+                        paddle.x = max(0, min(width - paddle_width, paddle.x))
+
+                        #ball movement
+                        ball.x += ball_vx
+                        ball.y += ball_vy
+
+                        #wall collision
+                        if ball.x - ball.radius <= 0:
+                            ball.x = ball.radius
+                            ball_vx *= -1
+
+                        if ball.x + ball.radius >= width:
+                            ball.x = width - ball.radius
+                            ball_vx *= -1
+
+                        if ball.y + ball.radius >= height:
+                            ball.y = height - ball.radius
+                            ball_vy *= -1
+
+                        #ball lost
+                        if ball.y < 0:
+                            end = True
+
+                        #paddle collision
+                        if (
+                            ball.y - ball.radius <= paddle.y + paddle.height
+                            and paddle.x <= ball.x <= paddle.x + paddle.width
+                            and ball_vy < 0
+                        ):
+                            ball_vy = abs(ball_vy)
+
+                            #add angle depending on hit position
+                            offset = (ball.x - (paddle.x + paddle.width / 2)) / (paddle.width / 2)
+                            ball_vx += offset * 15
+
+                        #brick collision
+                        hit_brick = None
+
+                        for brick in bricks:
+                            if (
+                                brick.x <= ball.x <= brick.x + brick.width
+                                and brick.y <= ball.y <= brick.y + brick.height
+                            ):
+                                hit_brick = brick
+                                score += 1
+                                break
+
+                        if hit_brick:
+                            bricks.remove(hit_brick)
+                            ball_vy *= -1
+                            if len(bricks) == 0:
+                                end = True
+
+                else:
+                    markers_detected = False
+
+            # Display the frame
+            win.clear()
+            img = cv2glet(frame, 'BGR')
+            img.blit(0, 0, 0)
+
+            if markers_detected:
+                paddle.draw()
+                ball.draw()
+                for brick in bricks:
+                    brick.draw()
+                    
+                #label to show current score
+                label_score = pyglet.text.Label(f'Score: {score}',
+                                                font_name='Pixel Game',
+                                                font_size=36,
+                                                color = (0, 0, 0),
+                                                x=10, y=10)
+                label_score.draw()
+
+    #end = True, ball got lost or all bricks were destroyed        
+    else:
         win.clear()
-        img = cv2glet(frame, 'BGR')
-        img.blit(0, 0, 0)
+        if len(bricks) == 0:
+            txt = "Congratulations, you got them all!"
+        else: 
+            txt = f'Game Over!  Score: {score}'
 
-        if markers_detected:
-            paddle.draw()
-            ball.draw()
-            for brick in bricks:
-                brick.draw()
+        label_end = pyglet.text.Label(txt,
+                            font_name='Pixel Game',
+                            font_size=36,
+                            x=width/2,
+                            y=height/2,
+                            anchor_x = "center",
+                            anchor_y = "center")
+        label_end.draw()
 
 pyglet.app.run()
